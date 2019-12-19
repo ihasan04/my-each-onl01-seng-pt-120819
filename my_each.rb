b90b0345acfbf45fd8b5af0ba2ac7597fd8b8f31
Ismael Hasan <ismael.hasan@live.com>
2019-12-19 15:35:11 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,11 @@
-def my_each(array)
-  if block_given?
-    i = 0
+# def my_each(array)
+#   if block_given?
+#     i = 0
  
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
-    array
-  end
-end
+#     while i < array.length
+#       yield(array[i])
+#       i = i + 1
+#     end
+#     array
+#   end
+# end
