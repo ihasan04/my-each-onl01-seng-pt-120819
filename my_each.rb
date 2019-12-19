d13d1cd728c3ed8d3dfe0e326e0157798154a071
Ismael Hasan <ismael.hasan@live.com>
2019-12-19 15:40:55 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -12,13 +12,13 @@
 
 def my_each(array)
   if block_given?
-  counter = 0
+    counter = 0
   
-  while counter < array.length
-    yield(array[counter])
-    counter += 1
+    while counter < array.length
+      yield(array[counter])
+      counter += 1
+    end
+    
+    array
   end
-  
-  array
-  
 end
