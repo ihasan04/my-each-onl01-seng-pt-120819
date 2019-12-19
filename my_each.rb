e72e89259908f2e708c198d61a03fb9d2169c04b
Ismael Hasan <ismael.hasan@live.com>
2019-12-19 15:35:16 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -9,3 +9,15 @@
 #     array
 #   end
 # end
+
+def my_each(array)
+  counter = 0
+  new_array = Array.new
+  while counter < array.length
+    new_array << yield(array[counter])
+    counter += 1
+  end
+  new_array
+end
+names = ["Max", "Jenny", "Thomas"]
+my_each(names) { |counter| puts counter }
